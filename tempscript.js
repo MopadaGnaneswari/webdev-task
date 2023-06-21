f8e82f28ed11f3ca85a646d09de7afd5a757234d
Mopada Gnaneswari <136959311+MopadaGnaneswari@users.noreply.github.com>
2023-06-21 18:29:41 +0530

    Update tempscript.js

--- tempscript.js
+++ tempscript.js
@@ -1,20 +1,20 @@
-let celsius = document.getElementById("celsius");
-let fahrenheit = document.getElementById("fahrenheit");
+const calculateTemp = ()=>
+    {
+    const inputTemp = document.getElementById('temp').value;
 
-function celToFar() 
-{
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
     
-    let output = (parseFloat(celsius.value) * 9 / 5) + 32;
-    fahrenheit.value = parseFloat(output.toFixed(2));
+    // let Celsius=0;
+    // let Fahrenheit=0;
 
-}
-
-
-function farToCel() 
-{
-    
-    let output = (parseFloat(fahrenheit.value) - 32) * 5 / 9;
-    celsius.value = parseFloat(output.toFixed(2));
-    console.log(output);
-
+    if (valueTemp=="cel"){
+        fahrenheit=(((inputTemp) * 9 / 5) + 32).toFixed(2);
+        document.getElementById("result1").innerHTML = fahrenheit + "&#176; Fahrenheit";
+    }
+    else if(valueTemp=="fah"){
+        celsius = (((inputTemp)-32)*5/9).toFixed(2);
+        document.getElementById("result1").innerHTML =  celsius+ "&#176; Celsius";
+       
+    }
 }
